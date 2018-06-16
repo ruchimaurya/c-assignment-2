53f7b1d93e1ea640872cc8992b852f2d1af610d6
Ruchi Maurya <ruchimaurya@promactinfo.com>
2018-06-16 12:24:32 +0530

    upload assignment

---
+++ inventory/inventory/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tot = 0, cnt = 0;
+            List<string> veglist = new List<string>();
+            veglist.Add("Tomato");
+            veglist.Add("Potato");
+            veglist.Add("ToMato");
+            veglist.Add("Potato");
+            veglist.Add("Red Tomato");
+            veglist.Add("Tomato  Catchup");
+            veglist.Add("Grapes");
+            veglist.Add("grapes");
+            veglist.Add("potato");
+
+            foreach (string str in veglist)
+            {
+                int ind = str.IndexOf("tomato", StringComparison.InvariantCultureIgnoreCase);
+
+                if (ind >= 0)
+                    tot = tot + 1;
+
+                if (str.Equals("tomato", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    cnt += 1;
+                }
+            }
+
+            Console.WriteLine("total string contains tomato : " + tot);
+            Console.WriteLine("types of string tomato : " + cnt);
+
+            foreach (var j in veglist)
+            {
+                if (j.Contains("Potato"))
+                {
+                    Console.WriteLine("index of Potato : {0} ", veglist.IndexOf(j));
+                }
+
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ numbers/numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 0.06f;
+            var y = 5.94f;
+            var sum = x + y;
+            Console.WriteLine("X + Y = {0}", sum);
+            
+
+            if (sum.Equals(6))
+                Console.WriteLine("X+Y!=6");
+            else
+                Console.WriteLine("X+Y==6");
+
+            if (x.CompareTo(y) > 0)
+                Console.WriteLine("X is greater");
+            else
+                Console.Write("Y is greater");
+
+            Console.Read();
+        }
+    }
+}
